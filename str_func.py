7a09c3ef3afde7ba40c01595a1323dd2e0a8b9f4
Ivsushko <113985732+Ivsushko@users.noreply.github.com>
2023-06-06 15:58:02 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,10 @@
 def func_upper():
+    '''
+    Принимает на вход строку, возвращает ее
+    со всеми заглавными буквами
+    '''
     s = input("Введите предложение: ")
     print(s.upper())
-
+    print(func_upper.__doc__)
 
 func_upper()
