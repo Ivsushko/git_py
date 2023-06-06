2b5581ba4cf839a74ae598f332cfc95849a9298f
Ivan Sushko <vaneksushko@gmail.com>
2023-06-06 16:51:46 +0300

    add func_first_upper

--- str_func.py
+++ str_func.py
@@ -2,5 +2,12 @@ def func_upper():
     s = input("Введите предложение: ")
     print(s.upper())
 
+def func_first_upper():
+    '''
+    Принимает на вход строку, возвращает ее
+    с заглавными первыми буквами каждого слова
+    '''
+    s = input("Введите предложение: ")
+    print(s.title())
+    print(func_upper.__doc__)
 
-func_upper()
